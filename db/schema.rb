320cc4974ce20409d2cba7c95cf4472ca0d64fe4
Mustapha <ait.mus06@gmail.com>
2022-11-23 14:19:11 +0100

    add relation between disbursment and orders

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_23_114332) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_23_125728) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -39,6 +39,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_23_114332) do
     t.bigint "shopper_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "disbursement_id"
+    t.index ["disbursement_id"], name: "index_orders_on_disbursement_id"
     t.index ["merchant_id"], name: "index_orders_on_merchant_id"
     t.index ["shopper_id"], name: "index_orders_on_shopper_id"
   end
@@ -52,6 +54,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_23_114332) do
   end
 
   add_foreign_key "disbursements", "merchants"
+  add_foreign_key "orders", "disbursements"
   add_foreign_key "orders", "merchants"
   add_foreign_key "orders", "shoppers"
 end
