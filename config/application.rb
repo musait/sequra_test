828b5c027d08919c8089fd7a31da68b70535b221
Mustapha <ait.mus06@gmail.com>
2022-11-25 12:40:07 +0100

    add configuration for rspec and factory_bot

--- config/application.rb
+++ config/application.rb
@@ -35,6 +35,8 @@ module SequraTest
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
+    config.factory_bot.definition_file_paths = ["spec/factories"]
+
     config.api_only = true
   end
 end
